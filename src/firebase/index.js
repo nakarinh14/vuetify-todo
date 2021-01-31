d759e3ccc71f6931fb278e9f018646f539afce14
nakarinh14 <nakarinh14@gmail.com>
2021-01-31 11:37:03 +0700

    Add Firebase Config

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+export default firebaseConfig;

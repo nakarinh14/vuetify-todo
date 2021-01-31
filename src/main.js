2b6dfa5f2289043bba667e5ae64213a1624a04cf
nakarinh14 <nakarinh14@gmail.com>
2021-01-31 11:38:26 +0700

    Update Project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import firebaseConfig from './firebase';
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  store.dispatch('auth/setUser', user);
+});
 
 new Vue({
   router,

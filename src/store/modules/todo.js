34565b57961be3529899883c8cec61523d2fb281
nakarinh14 <nakarinh14@gmail.com>
2021-02-08 22:22:18 +0700

    Add a new Firebase reference for config and moved listen to Vuex.

--- src/store/modules/todo.js
+++ src/store/modules/todo.js
@@ -1,34 +1,79 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import { sortedTodo } from '@/utils/formatTodo';
 
 export default {
   namespaced: true,
   state: {
     todos: null,
-    ref: null,
+    config: { hideComplete: false },
+    todosRef: null,
+    configRef: null,
   },
   mutations: {
     setTodo(state, todos) {
       state.todos = todos;
     },
-    setRef(state, ref) {
-      state.ref = ref;
+    setTodosRef(state, ref) {
+      state.todosRef = ref;
+    },
+    setConfigRef(state, ref) {
+      state.configRef = ref;
+    },
+    setConfig(state, config) {
+      state.config = config;
     },
   },
   actions: {
     setTodo({ commit }, todos) {
-      commit('setTodo', todos);
+      commit('setTodo', sortedTodo(todos));
     },
-    setRef({ commit }, userData) {
-      commit('setRef', userData ? firebase.database().ref(`/todo/${userData.uid}`) : null);
+    setConfig({ commit }, config) {
+      commit('setConfig', config);
+    },
+    setRef({ commit, dispatch }, userData) {
+      console.log('setting ref');
+      if (userData) {
+        console.log('user data exist');
+        const todosRef = firebase.database().ref(`/todo/${userData.uid}/todos`);
+        todosRef.on('value', (snapshot) => {
+          dispatch('setTodo', snapshot.val());
+        });
+        const configRef = firebase.database().ref(`/todo/${userData.uid}/config`);
+        configRef.on('value', (snapshot) => {
+          // Set default config if doesn't exist yet
+          if (!snapshot.exists()) {
+            configRef.set({
+              hideComplete: false,
+              hideActive: false,
+            });
+          } else {
+            dispatch('setConfig', snapshot.val());
+          }
+        });
+        commit('setTodosRef', todosRef);
+        commit('setConfigRef', configRef);
+      } else {
+        commit('setTodosRef', null);
+        commit('setConfigRef', null);
+      }
     },
   },
   getters: {
     todos(state) {
       return state.todos;
     },
-    ref(state) {
-      return state.ref;
+    hideComplete(state) {
+      return state.config.hideComplete;
+    },
+    hideActive(state) {
+      return state.config.hideActive;
+    },
+    todosRef(state) {
+      return state.todosRef;
+    },
+    configRef(state) {
+      return state.configRef;
     },
   },
 };

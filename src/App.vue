daec61115cecd96d39a0168c7f6ccd47d8cf4962
nakarinh14 <nakarinh14@gmail.com>
2021-02-09 11:30:57 +0700

    Add app bar for user profile and multiple todo lists

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
   <v-app>
-<!--    <v-app-bar>-->
-<!--      <v-spacer />-->
-<!--      <v-app-bar-title>-->
-<!--        <h1 class="font-weight-bold display-1">Yet Another Todo-List..</h1>-->
-<!--      </v-app-bar-title>-->
-<!--      <template v-slot:extension>-->
-<!--        <v-tabs-->
-<!--          centered-->
-<!--          background-color="blue-grey lighten-7"-->
-<!--          dark-->
-<!--          next-icon="mdi-arrow-right-bold-box-outline"-->
-<!--          prev-icon="mdi-arrow-left-bold-box-outline"-->
-<!--          show-arrows-->
-<!--        >-->
-<!--          <v-tab>Tab 1</v-tab>-->
-<!--          <v-tab>Tab 2</v-tab>-->
-<!--          <v-tab>Tab 3</v-tab>-->
-<!--        </v-tabs>-->
-<!--      </template>-->
-<!--    </v-app-bar>-->
-
-    <v-main
-      class="blue-grey lighten-5"
+    <v-app-bar
+      v-if="$store.getters['auth/user']"
+      color="blue-grey lighten-7"
+      dense
+      dark
+      app
     >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-title>Yet Another Todo List</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        dark
+      >
+        ADD TODOLIST
+      </v-btn>
+      <AvatarProfile></AvatarProfile>
+      <template v-slot:extension>
+        <v-tabs
+          centered
+          background-color="blue-grey lighten-7"
+          dark
+          show-arrows
+        >
+          <v-tab
+            v-for="i in 2"
+            :key="i"
+            :href="'#tab-' + i"
+          >
+            Item {{ i }}
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-main class="blue-grey lighten-5">
       <v-container fluid >
         <router-view></router-view>
       </v-container>
@@ -33,12 +44,10 @@
 
 <script>
 
+import AvatarProfile from '@/components/AvatarProfile.vue';
+
 export default {
   name: 'App',
+  components: { AvatarProfile },
 };
 </script>
-<style scoped>
-  .body{
-    background-color: grey;
-  }
-</style>
---
+++ src/components/AvatarProfile.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-menu
+    bottom
+    min-width="200px"
+    rounded
+    offset-y
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn
+        icon
+        v-on="on"
+      >
+        <v-avatar>
+          <v-icon dark>
+            mdi-account-circle
+          </v-icon>
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-list-item-content class="justify-center">
+        <div class="mx-auto text-center">
+          <p class="caption mt-1">
+            {{ userProfile.email }}
+          </p>
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            depressed
+            rounded
+            text
+            @click="onClickLogout"
+          >
+            LOGOUT
+          </v-btn>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AvatarProfile',
+  methods: {
+    onClickLogout() {
+      this.$router.push({ name: 'Login' });
+    },
+  },
+  computed: {
+    ...mapGetters({
+      userProfile: 'auth/user',
+    }),
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
